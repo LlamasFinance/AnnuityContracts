bf65a3e213b0f5d82a8c6e3cbe8181b1626ada1e
Himanshu bhatt <himanshubb109@gmail.com>
2022-05-08 01:38:51 +0530

    tested repay and withdraw

--- temp/utils.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from "hardhat";
-import { BigNumber, Contract } from "ethers";
-import { parseUnits } from "@ethersproject/units";
-
-const usdcDecimals = 6;
-
-const num = parseUnits("100", usdcDecimals);
-console.log(num);
